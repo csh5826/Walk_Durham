145424f791e0cd3bb0e7d0e61899c0bc3e07189a
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-17 13:22:17 -0400

    adds boiler plate html to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -27,6 +27,25 @@ import { bindActionCreators } from "redux";
         //this.blankFunction()
         return (
             <div>
+                <h1>Durham - get out there and walk!</h1>
+                <div className="destinations">
+                    <h3>Where do you want to go?</h3>
+                    <ul>
+                        <li>the pub</li>
+                        <li>the park</li>
+                        <li>a museum</li>
+                    </ul>
+                </div>
+                <div className="distance">
+                    <h3>How dar do you want to walk?</h3>
+                    <ul>
+                        <li>1 mile</li>
+                        <li>3 mile</li>
+                        <li>5 mile</li>
+                    </ul>
+
+                    <button>let's walk!</button>
+                </div>
 
             </div>
         )
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,1 +1,7 @@
 // incapsulates our entire /details page
+
+import React from 'react';
+import { Component } from 'react';
+import { fetchDestination } from '../actions';
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
