57ca98b6b8b71f13db6edbf1de4a7af615adb812
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-17 16:26:46 -0400

    adding radio buttons to our app component

--- src/components/App.js
+++ src/components/App.js
@@ -24,28 +24,108 @@ import { bindActionCreators } from "redux";
      this.props.history.push("/details")
    }
 
-
-
     render() {
-        //this.blankFunction()
         return (
             <div>
                 <h1>Durham - get out there and walk!</h1>
                 <div className="destinations">
                     <h3>Where do you want to go?</h3>
-                    <ul>
-                        <li>the pub</li>
-                        <li>the park</li>
-                        <li>a museum</li>
-                    </ul>
+                    <form>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="bars"
+                                checked={this.state.selectedOption === "option1"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            the pub
+                            </label>
+                        </div>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="gardens_and_parks"
+                                checked={this.state.selectedOption === "option2"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            the park
+                            </label>
+                        </div>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="museums"
+                                checked={this.state.selectedOption === "option3"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            a museum
+                            </label>
+                        </div>
+
+
+                    </form>
+                    
                 </div>
                 <div className="distance">
                     <h3>How far do you want to walk?</h3>
-                    <ul>
-                        <li>1 mile</li>
-                        <li>3 mile</li>
-                        <li>5 mile</li>
-                    </ul>
+                    <form>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="1610"
+                                checked={this.state.selectedOption === "option1"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            1 mile
+                            </label>
+                        </div>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="4828"
+                                checked={this.state.selectedOption === "option2"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            3 miles
+                            </label>
+                        </div>
+
+                        <div className="form-check">
+                            <label>
+                            <input
+                                type="radio"
+                                name="react-tips"
+                                value="8047"
+                                checked={this.state.selectedOption === "option3"}
+                                onChange={this.handleOptionChange}
+                                className="form-check-input"
+                            />
+                            5 miles
+                            </label>
+                        </div>
+
+
+                    </form>
 
                     <button onClick={this.handleClick}>let's walk!</button>
                 </div>
